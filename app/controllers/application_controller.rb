4050db183e3a7c2b400bc6fb5aa90fb8c2c0bc4d
Yuuki <ukmmm0523@gmail.com>
2021-07-20 22:54:14 +0900

    ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :login_required
   private
   def login_required
-   redirect_to new_session_path unless current_user
-   end
+    redirect_to new_session_path unless current_user
   end
+end
